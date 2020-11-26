26c512a21923562e5a7d26bc592e519efc1555e3
Luka Železnik <luka.zeleznik10@gmail.com>
2020-11-27 00:58:33 +0100

    styling

--- nginx/www/opentravian/src/components/Main/Navbar/Resources.vue
+++ nginx/www/opentravian/src/components/Main/Navbar/Resources.vue
@@ -74,41 +74,41 @@ export default {
             });
         },
         startIntervals(){
-        var woodInterval = setInterval( ()=> {
-            if(parseInt(this.villageResources[0]) < this.villageMaxResources[0]){
-            this.$set(this.villageResources, 0, this.villageResources[0]+1);
-            }
-            else if(this.villageResources[0] == this.villageMaxResources[0]){
-            clearInterval(woodInterval);
-            }
-        }, 1000*3600 / this.villageProduction[0]);
+            var woodInterval = setInterval( ()=> {
+                if(parseInt(this.villageResources[0]) < this.villageMaxResources[0]){
+                    this.$set(this.villageResources, 0, this.villageResources[0]+1);
+                }
+                else if(this.villageResources[0] == this.villageMaxResources[0]){
+                    clearInterval(woodInterval);
+                }
+            }, 1000*3600 / this.villageProduction[0]);
 
-        var clayInterval = setInterval( ()=> {
-            if(parseInt(this.villageResources[1]) < this.villageMaxResources[1]){
-            this.$set(this.villageResources, 1, this.villageResources[1]+1);
-            }
-            else if(this.villageResources[1] == this.villageMaxResources[1]){
-            clearInterval(clayInterval);
-            }
-        }, 1000*3600 / this.villageProduction[1]);
+            var clayInterval = setInterval( ()=> {
+                if(parseInt(this.villageResources[1]) < this.villageMaxResources[1]){
+                    this.$set(this.villageResources, 1, this.villageResources[1]+1);
+                }
+                else if(this.villageResources[1] == this.villageMaxResources[1]){
+                    clearInterval(clayInterval);
+                }
+            }, 1000*3600 / this.villageProduction[1]);
 
-        var ironInterval = setInterval( ()=> {
-            if(parseInt(this.villageResources[2]) < this.villageMaxResources[2]){
-            this.$set(this.villageResources, 2, this.villageResources[2]+1);
-            }
-            else if(this.villageResources[2] == this.villageMaxResources[2]){
-            clearInterval(ironInterval);
-            }
-        }, 1000*3600 / this.villageProduction[2]);
-            
-        var cropInterval = setInterval( ()=> {
-            if(parseInt(this.villageResources[3]) < this.villageMaxResources[3]){
-            this.$set(this.villageResources, 3, this.villageResources[3]+1);
-            }
-            else if(this.villageResources[3] == this.villageMaxResources[3]){
-            clearInterval(cropInterval);
-            }
-        }, 1000*3600 / this.villageProduction[3]);
+            var ironInterval = setInterval( ()=> {
+                if(parseInt(this.villageResources[2]) < this.villageMaxResources[2]){
+                    this.$set(this.villageResources, 2, this.villageResources[2]+1);
+                }
+                else if(this.villageResources[2] == this.villageMaxResources[2]){
+                    clearInterval(ironInterval);
+                }
+            }, 1000*3600 / this.villageProduction[2]);
+                
+            var cropInterval = setInterval( ()=> {
+                if(parseInt(this.villageResources[3]) < this.villageMaxResources[3]){
+                    this.$set(this.villageResources, 3, this.villageResources[3]+1);
+                }
+                else if(this.villageResources[3] == this.villageMaxResources[3]){
+                    clearInterval(cropInterval);
+                }
+            }, 1000*3600 / this.villageProduction[3]);
         },
     }
 }
