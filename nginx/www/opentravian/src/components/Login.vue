83d74c99e010b137dede2d26b0883e7b2392bf1f
David Delac <delacdavid@gmail.com>
2020-12-04 16:11:50 +0100

    Update Login.vue

--- nginx/www/opentravian/src/components/Login.vue
+++ nginx/www/opentravian/src/components/Login.vue
@@ -1,18 +1,45 @@
 <template>
     <div class="container text-center">
-        <form class="form-group w-50 m-auto" method="POST">
+        <form class="form-group w-50 m-auto" id="loginForm" action="#">
             <h2 class="form-signin-heading" >Login</h2>
             <input type="text" name="username" class="form-control w-100 my-2 username-field input-lg" placeholder="Username" required autofocus>
             <input type="password" name="password" id="inputPassword" class="form-control w-100 mb-2" placeholder="Password" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            <button class="btn btn-lg btn-primary btn-block" v-on:click="login()">Login</button>
             <a class="btn btn-lg btn-primary btn-block" href="/register">Register</a>
         </form>
     </div>
 </template>
 
 <script>
+import { apiRequestMixins } from '@/mixins/apiRequestMixins' 
     export default {
-        mounted() {
+        name: 'Login',
+        data() {
+            return {
+            }
+        },
+
+        mixins: [apiRequestMixins],
+
+        methods: {
+            async login() {
+                const loginForm = document.getElementById('loginForm');
+                const username  = loginForm.querySelector('input[name=username]').value;
+                const password  = loginForm.querySelector('input[name=password]').value;
+                let current = this;
+
+                loginForm.addEventListener('submit', async function(e){
+                    e.preventDefault();
+                    if(username != "" && password != "") {
+                        let userResponse = await current.doApiRequest("login?email=" + username + "&password=" + password, "POST", "");
+                        let userResponseJson = await userResponse.json();
+                        console.log(userResponseJson.token);
+                    } else {
+                        console.log("A username and password must be present");
+                    }
+                });
+            }
         }
     }
+
 </script>
